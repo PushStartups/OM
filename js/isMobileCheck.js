26ad655b46afad1c2c3b0a9ae0268f052382e8fa
Muhammad Iftikhar <muhammad.iftikhar.aftab@gmail.com>
2017-03-05 12:00:49 +0500

    Image for Items hide

--- js/isMobileCheck.js
+++ js/isMobileCheck.js
@@ -1,21 +1,27 @@
 var isMobile = {
 
     Android: function() {
+
         return navigator.userAgent.match(/Android/i);
     },
     BlackBerry: function() {
+
         return navigator.userAgent.match(/BlackBerry/i);
     },
     iOS: function() {
+
         return navigator.userAgent.match(/iPhone|iPad|iPod/i);
     },
     Opera: function() {
+
         return navigator.userAgent.match(/Opera Mini/i);
     },
     Windows: function() {
+
         return navigator.userAgent.match(/IEMobile/i);
     },
     any: function() {
+
         return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
     }
 };
@@ -25,18 +31,26 @@ if(!isMobile.any())
 {
     if(window.location.hostname == "dev.m.orderapp.com")
     {
+
         window.location.href = 'http://dev.landing.orderapp.com';
+
     }
     else if(window.location.hostname == "qa.m.orderapp.com")
     {
+
         window.location.href = 'http://qa.landing.orderapp.com';
+
     }
     else if(window.location.hostname == "staging.m.orderapp.com")
     {
+
         window.location.href = 'http://staging.orderapp.com';
+
     }
     else
     {
+
         window.location.href = 'http://orderapp.com';
+
     }
 }
