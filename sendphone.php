e7e60a6a9454fd3ec4988136419b51698d76817f
RaelOlwyn <rael@pushstartups.com>
2017-03-21 16:27:14 +0200

    Update sendphone.php

--- sendphone.php
+++ sendphone.php
@@ -1,5 +1,5 @@
 <?php
-$file = '/var/www/html/qa.website_2.0/en/phonenumbers.txt';
+$file = '/var/www/html/staging.orderapp.com/phonenumbers.txt';
 $phone = $_REQUEST['phone'];
 file_put_contents($file, $phone, FILE_APPEND | LOCK_EX);
 ?>
