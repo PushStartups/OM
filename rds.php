048d37981ad5683156515d8e34cb146bdfe3eb04
Muhammad Iftikhar <muhammad.iftikhar.aftab@gmail.com>
2017-04-30 12:19:31 +0500

    DB update + code latest

--- rds.php
+++ rds.php
@@ -1,20 +1,47 @@
 <?php
 
-define("HOST", "orderappdev.crv4lzhgi1gx.eu-central-1.rds.amazonaws.com:3306");
-define("DBUSER", "root");
-define("PASS", "orderapp");
-define("DB", "orderapp");
-define("PORT", 3306);
+// TAKE DB DUMP AND SAVE WITH DATE TIME STAMP
+
+$name =  date("Y_m_d");
+$return_var = NULL;
+$output = NULL;
+
+try {
+
+    $result = exec('mysqldump --replace --user=root --password=orderapp orderapp > /var/www/html/db_backups/' . $name . '.sql', $output, $return_var);
+
+    var_dump($output);
+    var_dump($return_var);
+
+    $result = exec('mysql -h orderappdev.crv4lzhgi1gx.eu-central-1.rds.amazonaws.com -P 3306 -u root --password=orderapp  orderapp < /var/www/html/db_backups/' . $name . '.sql', $output, $return_var);
+
+    var_dump($output);
+    var_dump($return_var);
 
 
-$link = mysqli_connect(HOST,DBUSER,PASS,DB,PORT);
-
-
-if (mysqli_connect_errno($link)){
-
-    echo "Failure to connect: " . mysqli_connect_error();
 }
-else{
+catch (Exception $exp)
+{
 
-    echo "successfully connected";
+    echo  $exp;
+
 }
+
+//define("HOST", "orderappdev.crv4lzhgi1gx.eu-central-1.rds.amazonaws.com:3306");
+//define("DBUSER", "root");
+//define("PASS", "orderapp");
+//define("DB", "orderapp");
+//define("PORT", 3306);
+//
+//
+//$link = mysqli_connect(HOST,DBUSER,PASS,DB,PORT);
+//
+//
+//if (mysqli_connect_errno($link)){
+//
+//    echo "Failure to connect: " . mysqli_connect_error();
+//}
+//else{
+//
+//    echo "successfully connected";
+//}
