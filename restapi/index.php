19d5d2178171b303f0713d11a2aae13529146cef
Muhammad Iftikhar <muhammad.iftikhar.aftab@gmail.com>
2017-05-18 13:32:16 +0500

    DB update + code latest

--- restapi/index.php
+++ restapi/index.php
@@ -986,7 +986,22 @@ $app->post('/add_order', function ($request, $response, $args) {
 
 });
 
+$app->post('/send_email_to_b2b_users', function ($request, $response, $args) {
 
+    $email          = $request->getParam('email');
+    $password       = $request->getParam('password');
+    $user_name      = $request->getParam('user_name');
+
+    email_to_b2b_users($email,$password,$user_name);
+
+    ob_end_clean();
+
+    // RESPONSE RETURN TO REST API CALL
+    $response = $response->withStatus(202);
+    $response = $response->withJson(json_encode('success'));
+    return $response;
+
+});
 
 $app->post('/tcs_printer', function ($request, $response, $args) {
 
@@ -1245,6 +1260,96 @@ function  stripePaymentRequest($amount,$user_order,$user_id,$creditCardNo,$expDa
 
     return $rest;
 }
+
+//SEND EMAIL TO B2B USERS CREDENTIALS INFO
+function email_to_b2b_users($email,$password,$username)
+{
+
+    $mailbody = '<html lang="en">
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>OrderApp</title>';
+
+    //    <style type="text/css">
+    //    </style>
+    $mailbody .= '</head><body bgcolor="#f7f7f7" style="background: #f7f7f7;">';
+    $mailbody .= '<table border="0" cellpadding="0" cellspacing="0" width="600" id="templateColumns" style="color: #000; font-size: 16px; line-height: 18px; font-weight: 400; width: 600px; margin: 0 auto; font-family: Arial, Helvetica, sans-serif;">';
+    $mailbody .= '<tr><td align="left" valign="top" width="100%" bgcolor="#ff7f00" style="background: #ff7f00; font-size: 18px; line-height: 22px; font-weight: 700;padding: 20px;">';
+    $mailbody .= '<img style="display: inline-block; vertical-align: middle; margin: 0;" src="https://dev.orderapp.com/admin/img/email-logo.png" alt="orderapp">';
+    $mailbody .= '<h1 style="display: inline-block; vertical-align: middle; margin: 0 10px; font-weight: 400; color: #fff;">Welcome to OrderApp!</h1></td>';
+    $mailbody .= '</tr>';
+    $mailbody .= '<tr>';
+    $mailbody .= '<td align="left" valign="top" width="100%" bgcolor="#FFFFFF" style="padding: 50px 25px; background: #fff;">';
+    $mailbody .= '<p>Hi '.$username.' <br>Your login details are as follows:</p>';
+    $mailbody .= '<p><b>Username : </b> '.$username.'</p>';
+    $mailbody .= '<p><b>Password : </b> '.$password.'</p><br>';
+    $mailbody .= '<p>Visit Website :<a style="color: #3b5998; text-decoration: none;" href="#">https://devb2b.orderapp.com/en/</a></p>';
+    $mailbody .= '</td>';
+    $mailbody .= '</tr>';
+    $mailbody .= '<tr>';
+    $mailbody .= '<td align="center" style="padding: 100px 0 20px;">';
+    $mailbody .= '<table border="0" cellspacing="0" cellpadding="0">';
+    $mailbody .= '<tr><td width="37" style="text-align: center; padding: 0 8px;"><a href="#"><img src="https://dev.orderapp.com/admin/img/fb.png" width="37" height="37" alt="Facebook" border="0" /></a></td>';
+    $mailbody .= '<td width="37" style="text-align: center; padding: 0 8px;"><a href="#"><img src="https://dev.orderapp.com/admin/img/tw.png" width="37" height="37" alt="Twitter" border="0" /></a></td>';
+    $mailbody .= '<td width="37" style="text-align: center; padding: 0 8px;"><a href="#"> <img src="https://dev.orderapp.com/admin/img/gp.png" width="37" height="37" alt="Facebook" border="0" /></a></td>';
+    $mailbody .= '<td width="37" style="text-align: center; padding: 0 8px;"><a href="#"><img src="https://dev.orderapp.com/admin/img/insta.png" width="37" height="37" alt="Twitter" border="0" /></a></td>';
+    $mailbody .= '<td width="37" style="text-align: center; padding: 0 8px;"><a href="#"><img src="https://dev.orderapp.com/admin/img/pin.png" width="37" height="37" alt="Facebook" border="0" /></a></td>';
+    $mailbody .= '<td width="37" style="text-align: center; padding: 0 8px;"><a href="#"><img src="https://dev.orderapp.com/admin/img/link.png" width="37" height="37" alt="Twitter" border="0" /></a></td>';
+    $mailbody .= '</tr>';
+    $mailbody .= '</table>';
+    $mailbody .= '</td>';
+    $mailbody .= '</tr>';
+    $mailbody .= '<tr>';
+    $mailbody .= '<td><p style="margin: 0; text-align: center;"><a style="color: #797979; text-decoration: none;" href="https://dev.orderapp.com/en/">orderapp.com</a></p></td></tr></table></body></html>';
+
+    $mail = new PHPMailer;
+
+    $mail->CharSet = 'UTF-8';
+
+    $mail->SMTPDebug = 3;                                               // Enable verbose debug output
+
+    $mail->isSMTP();
+    $mail->Host = "email-smtp.eu-west-1.amazonaws.com";                 //   Set mailer to use SMTP
+    $mail->SMTPAuth = true;                                             //   Enable SMTP authentication
+    $mail->Username = "AKIAJZTPZAMJBYRSJ27A";
+    $mail->Password = "AujjPinHpYPuio4CYc5LgkBrSRbs++g9sJIjDpS4l2Ky";   //   SMTP password
+    $mail->SMTPSecure = 'tls';                                          //   Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;
+
+    //From email address and name
+    $mail->From = "order@orderapp.com";
+    $mail->FromName = "OrderApp";
+
+
+    //To address and name
+    $mail->addAddress($email);                    //SEND ADMIN EMAIL
+
+
+    //Address to which recipient will reply
+    $mail->addReplyTo("order@orderapp.com", "Reply");
+
+
+    //Send HTML or Plain Text email
+    $mail->isHTML(false);
+    $mail->Subject = "B2B OrderApp Credentials Info";
+    $mail->Body = $mailbody;
+    $mail->AltBody = "OrderApp";
+
+    if (!$mail->send())
+    {
+        echo "Mailer Error: " . $mail->ErrorInfo;
+    }
+    else
+    {
+        echo "Message has been sent successfully";
+    }
+
+}
+
+
 
 
 // STRIPE PAYMENT REQUEST HE
