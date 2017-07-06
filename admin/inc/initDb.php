c108246caa8c4d26096cc1c4d26c34b49200b79d
M.Iftikhar <muhammad.iftikhar.aftab@gmail.com>
2017-07-06 14:01:37 +0500

    update eluna and app.orderapp on RDS

--- admin/inc/initDb.php
+++ admin/inc/initDb.php
@@ -4,11 +4,12 @@
 
 require_once 'meekrodb.2.3.class.php';
 
-if($_SERVER['HTTP_HOST'] == "orderapp.com") {
+if($_SERVER['HTTP_HOST'] == "orderapp.com" || $_SERVER['HTTP_HOST'] == "app.orderapp.com" || $_SERVER['HTTP_HOST'] == "eluna.orderapp.com") {
 
     DB::$host = 'production-orderapp.crv4lzhgi1gx.eu-central-1.rds.amazonaws.com';
     DB::$port = '3306';
 }
+
 DB::$user = 'root';
 DB::$password = 'orderapp';
 DB::$dbName = 'orderapp_restaurants';
