bb91882a55a52e3020631e0aaeeaabfb0b001535
M.Iftikhar <muhammad.iftikhar.aftab@gmail.com>
2017-08-02 13:18:01 +0500

    updated db dump

---
+++ D/index.php
@@ -0,0 +1,56 @@
+<?php
+
+require "../restapi/inc/initDb.php";
+
+$url         =      explode("/", $_SERVER['REQUEST_URI']);
+$agent_name  =      end($url);
+
+DB::useDB("orderapp_user");
+$agent = DB::queryFirstRow("select * from agents where name = '$agent_name'");
+
+if(DB::count() == 0)
+{
+    DB::useDB("orderapp_user");
+    DB::insert('agents', array(
+        "name"              =>  $agent_name,
+        "sales_count"       =>  1,
+
+    ));
+}
+else
+{
+    $total = $agent['sales_count'] + 1;
+    DB::useDB("orderapp_user");
+    DB::update('agents', array(
+        "sales_count"               =>      $total,
+
+    ), "name=%s", $agent_name
+    );
+}
+
+$iPod    = stripos($_SERVER['HTTP_USER_AGENT'],"iPod");
+$iPhone  = stripos($_SERVER['HTTP_USER_AGENT'],"iPhone");
+$iPad    = stripos($_SERVER['HTTP_USER_AGENT'],"iPad");
+$Android = stripos($_SERVER['HTTP_USER_AGENT'],"Android");
+$webOS   = stripos($_SERVER['HTTP_USER_AGENT'],"webOS");
+
+if( $iPod || $iPhone || $iPad)
+{
+    $redirect_url = "https://itunes.apple.com/us/app/orderapp-customer/id1148884312?mt=8";
+    header("Location: $redirect_url");
+}
+else if($Android)
+{
+    $redirect_url = "https://play.google.com/store/apps/details?id=ps.orderapp&hl=en";
+    header("Location: $redirect_url");
+}
+else if($webOS)
+{
+    $redirect_url = "https://orderapp.com";
+    header("Location: $redirect_url");
+}
+else
+{
+    $redirect_url = "https://orderapp.com";
+    header("Location: $redirect_url");
+}
